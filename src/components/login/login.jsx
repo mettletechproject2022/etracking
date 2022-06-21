7cebf0ba64b250e0c4146028907e79094e4f2623
LAPTOP-41L0LH0B\Raj <mettletech.employee@gmail.com>
2022-06-21 21:30:49 +0530

    login template added

---
+++ src/components/login/login.jsx
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import logo from "./assets/logo.png";
+import "./login.css";
+
+class login extends Component {
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(e.target.email.value);
+
+    if (!e.target.email.value) {
+      alert("Email is required");
+    } else if (!e.target.email.value) {
+      alert("Valid email is required");
+    } else if (!e.target.password.value) {
+      alert("Password is required");
+    } else if (
+      e.target.email.value === "me@example.com" &&
+      e.target.password.value === "123456"
+    ) {
+      alert("Successfully logged in");
+      e.target.email.value = "";
+      e.target.password.value = "";
+    } else {
+      alert("Wrong email or password combination");
+    }
+  };
+
+  handleClick = e => {
+    e.preventDefault();
+
+    alert("Goes to registration page");
+  };
+
+  render() {
+    return (
+      <div className="login">
+        <img src={logo} className="logo" alt="MettleTech--User/Admin" />
+        <form className="form" onSubmit={this.handleSubmit}>
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" placeholder="name@email.com.br" required= {true} />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" />
+          </div>
+          <button className="primary">Log in</button>
+        </form>
+        <button className="secondary" onClick={this.handleClick}>
+          Create A New Account
+        </button>
+      </div>
+    );
+  }
+}
+
+export default login;
