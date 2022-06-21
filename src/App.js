03b6820ba86744f069590de50e94cce1997863e3
Haranath Chakrabarty <haranath15@gmail.com>
2022-06-21 21:45:31 +0530

    haranath

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Login from './components/login/login';
 function App() {
   return (
     <div className="App">
-    {/* <h1>Hi there! mettle tech project 2023</h1>
+    {/* <h1>Hi there! mettle tech project 2022</h1>
     <h2>hi! there buddy here...</h2> */}
     <Login/>
     </div>
