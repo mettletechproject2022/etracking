e6650d281c9a5930c79bfb18b7fc93a4ef28bed3
LAPTOP-41L0LH0B\Raj <mettletech.employee@gmail.com>
2022-06-22 22:51:01 +0530

    login changed and forgot password added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './components/login/login';
+import Home from './components/home/home';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
 
 function App() {
   return (
@@ -9,7 +11,14 @@ function App() {
     {/* <h1>Hi there! This is mettle tech project 2022</h1>
     <h2>hi! there buddy here...</h2>
     <h2>hi! there buddy here...</h2> */}
-    <Login/>
+    <BrowserRouter>
+    <Routes>
+      <Route exact path="/" element={<Login/>}/>
+      <Route path="/home" element={<Home/>}/>
+
+    </Routes>
+    </BrowserRouter>
+    
     </div>
   );
 }
