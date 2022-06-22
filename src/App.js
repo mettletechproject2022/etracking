9001fdfe88bbb8f1422fc0522b08f1ee0f34e05d
Ratnadeep Dey <mettletech.employee@gmail.com>
2022-06-22 11:24:51 +0530

    VeryGood

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Login from './components/login/login';
 function App() {
   return (
     <div className="App">
-   <h1>Hello I am Ratnadeep</h1>
+   <h1>Hello I am Ratnadeep Dey</h1>
     {/* <h1>Hi there! This is mettle tech project 2022</h1>
     <h2>hi! there buddy here...</h2> */}
     <Login/>
