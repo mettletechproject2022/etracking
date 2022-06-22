bcb80be6d16b5683e98ae2a9c63ac89afc4f618e
KoustavDas22 <mettletech.employee@gmail.com>
2022-06-22 13:52:07 +0530

    new upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from './components/login/login';
+
+function App() {
+  return (
+    <div className="App">
+   <h1>Hello I am Koustav...I am employee</h1>
+    {/* <h1>Hi there! This is mettle tech project 2022</h1>
+    <h2>hi! there buddy here...</h2> */}
+    <Login/>
+    </div>
+  );
+}
+
+export default App;
