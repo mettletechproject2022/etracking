a142e0f76b3e620460974f45a0700e58da9207b0
subhajithooghly <mettletech.employee@gmail.com>
2022-06-23 13:26:27 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './components/login/login';
+import ManageUser from './components/manageuser/ManageUser';
 
 function App() {
   return (
     <div className="App">
-    <h1>Hello I am Ratnadeep Dey 2022-23</h1> 
+    {/* <h1>Hello I am Ratnadeep Dey 2022-23</h1>  */}
     {/* <h1>Hi there! This is mettle tech project 2022</h1>
     <h2>hi! there buddy here...</h2>
     <h2>hi! there buddy here...</h2> */}
     <Login/>
+    <ManageUser />
     </div>
   );
 }
