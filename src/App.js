82c484740f376de2713a689ee8973af905acccec
subhajithooghly <mettletech.employee@gmail.com>
2022-06-25 10:36:28 +0530

    navbar css change

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import OTP from "./components/login/otp/otp";
 import Popupbutt from "./components/login/popUpButt/popUpButt";
 import Register from "./components/register/register";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import ManageUser from './components/manageuser/ManageUser';
+import ManageUser from './components/manageuser/ManageUser';
 import Data_visualization from './components/data_visualization/data_visualization';
 import Dev1 from './components/data_visualization/imageDisplay/dev1';
 
@@ -18,7 +18,7 @@ import Dashboard from "./pages/Dashboard.jsx";
 import DataVisualisation from "./pages/DataVisualisation";
 import History from "./pages/History";
 import Logout from "./pages/Logout";
-import ManageUser from "./pages/ManageUser";
+// import ManageUser from "./pages/ManageUser";
 import Notification from "./pages/Notification";
 import Profile from "./pages/Profile";
 import "./App.css";
@@ -37,7 +37,7 @@ function App() {
       <Route path='/popupbutt' element={<Popupbutt/>}/>
       <Route path="/data_visualization" element={<Data_visualization/>}/>
       <Route path="/dev1" element={<Dev1/>}/>
-      <Route path="/manageuser" element={<ManageUser/>}/>
+      <Route path="/manageusers" element={<ManageUser/>}/>
       <Route path="/register" element={<Register/>}/>
       <Route path="/sidebar" element={<Sidebar />}/>
 
@@ -46,7 +46,7 @@ function App() {
       <Route path="/dataVisualisation" element={<DataVisualisation/>}/>
       <Route path="/history" element={<History/>}/>
       <Route path="/logout" element={<Logout/>}/>
-      <Route path="/manageUser" element={<ManageUser/>}/>
+      {/* <Route path="/manageUser" element={<ManageUser/>}/> */}
       <Route path="/notification" element={<Notification/>}/>
       <Route path="/profile" element={<Profile/>}/>
       </Routes>
@@ -62,7 +62,7 @@ function App() {
           <Route path="/otp" element={<OTP />} />
           <Route path="/popupbutt" element={<Popupbutt />} />
           {/* <Route path="/data_visualization" element={<Data_visualization/>}/> */}
-          <Route path="/manageuser" element={<ManageUser />} />
+          {/* <Route path="/manageuser" element={<ManageUser />} /> */}
           <Route path="/register" element={<Register />} />
         </Routes>
       </BrowserRouter>
