be2f1250472ebbc7b6f2f1e055dedb25392cf4ae
mondal <mettletech.employee@gmail.com>
2022-06-27 09:23:46 +0530

    changes

--- src/pages/DataVisualisation.jsx
+++ src/pages/DataVisualisation.jsx
@@ -13,35 +13,35 @@ const DataVisualisation = () => {
       <h1>DATA VISUALISATION</h1>
       <div className="sec">
           <img src={file} className='moji' alt="images" /><br />
-          <a href="dev1"> developer 1 </a>
+          <a href="dev1"> Developer 1 </a>
       </div>
       <div className="sec">
       <img src={file} className='moji' alt="images" /><br />
-          <a href="dev2"> developer 2 </a>
+          <a href="dev2"> Developer 2 </a>
       </div>
       <div className="sec">
       <img src={file} className='moji' alt="images" /><br />
-          <a href="#"> developer 3 </a>
+          <a href="#"> Developer 3 </a>
       </div>
       <div className="sec">
       <img src={file} className='moji' alt="images" /><br />
-          <a href="#"> developer 4 </a>
+          <a href="#"> Developer 4 </a>
       </div>
       <div className="sec">
       <img src={file} className='moji' alt="images" /><br />
-          <a href="#"> developer 5 </a>
+          <a href="#"> Developer 5 </a>
       </div>
       <div className="sec">
       <img src={file} className='moji' alt="images" /><br />
-          <a href="#"> developer 6 </a>
+          <a href="#"> Developer 6 </a>
       </div>
       <div className="sec">
       <img src={file} className='moji' alt="images" /><br />
-          <a href="#"> developer 7 </a>
+          <a href="#"> Developer 7 </a>
       </div>
       <div className="sec">
       <img src={file} className='moji' alt="images" /><br />
-          <a href="#"> developer 8 </a>
+          <a href="#"> Developer 8 </a>
       </div>
      
       </div>
