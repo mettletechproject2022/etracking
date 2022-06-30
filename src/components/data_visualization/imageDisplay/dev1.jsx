c856823fff31661412d698214559c1aea3370b7a
Ratnadeep Dey <mettletech.employee@gmail.com>
2022-06-30 23:52:49 +0530

    data

--- src/components/data_visualization/imageDisplay/dev1.jsx
+++ src/components/data_visualization/imageDisplay/dev1.jsx
@@ -1,29 +1,29 @@
-
 import "./dev.css";
 import Sidebar from "../../../components/sidebar/Sidebar";
 import "../../sidebar/sidebar.css";
 import "../../../App.css";
 // import { Modal } from "bootstrap";
-import { Modal, ModalHeader,ModalBody,Row,Col} from "reactstrap";
+import { Modal, ModalHeader, ModalBody, Row, Col } from "reactstrap";
 import { useState } from "react";
-// import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Dev1 = () => {
   const [modal, setmodal] = useState(false)
 
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
 
+  const history = useNavigate()
 
   const success = () => {
     alert('Message sent successfully')
-    // navigate("/dev2");
+    navigate("/dev1");
   };
 
   return (
     <>
       <button className="butt" onClick={() => history(-1)}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
-</svg></button>
+        <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z" />
+      </svg></button>
       <div className="container-fluid9 p-0 d-flex flex-row">
         <Sidebar />
         <div className="con">
@@ -35,49 +35,49 @@ const Dev1 = () => {
                 <a href="images/Dev1/1648560446131.png" target="_blank">Zoom</a>
 
                 {/* Pop-up Message code */}
-              {/* <div> */}
-              <Modal
-                size='lg'
-                isOpen={modal}
-                toggle={() => setmodal(!modal)}
-              >
-
-                <ModalHeader
+                {/* <div> */}
+                <Modal
+                  size='lg'
+                  isOpen={modal}
                   toggle={() => setmodal(!modal)}
                 >
-                  Comments
-                </ModalHeader>
 
-                <ModalBody>
-                  <form action="">
-                    <Row>
-                      <Col lg={12}>
-                        <div>
-                          <label htmlFor="name">Name:</label>
-<input type="text" className="form-control" placeholder="enter name"/>
-                        </div>
-                      </Col>
-                    </Row>
+                  <ModalHeader
+                    toggle={() => setmodal(!modal)}
+                  >
+                    Comments
+                  </ModalHeader>
 
-                    <Row>
-                      <Col lg={12}>
-                        <div>
-                          <label htmlFor="name">Message:</label>
-<input type="text" className="form-control" placeholder="enter message"/>
-                        </div>
-                      </Col>
-                    </Row>
-                  </form>
-                  <button className="button mt-3" style={{color: "green"}} onClick={()=>success()}>Submit</button>
-                </ModalBody>
+                  <ModalBody>
+                    <form action="">
+                      <Row>
+                        <Col lg={12}>
+                          <div>
+                            <label htmlFor="name">Name:</label>
+                            <input type="text" className="form-control" placeholder="enter name" />
+                          </div>
+                        </Col>
+                      </Row>
 
-              </Modal>
+                      <Row>
+                        <Col lg={12}>
+                          <div>
+                            <label htmlFor="name">Message:</label>
+                            <input type="text" className="form-control" placeholder="enter message" />
+                          </div>
+                        </Col>
+                      </Row>
+                    </form>
+                    <button className="button mt-3" style={{ color: "green" }} onClick={() => success()}>Submit</button>
+                  </ModalBody>
+
+                </Modal>
 
                 <a href="#" className="button" onClick={() => setmodal(true)}>Comments</a>
 
-              {/* </div> */}
+                {/* </div> */}
 
-              {/* pop-up message code end */}
+                {/* pop-up message code end */}
               </div>
             </div>
           </div>
