b243fcd194eafa2886db06e165eb9792c5cffb52
Ratnadeep Dey <mettletech.employee@gmail.com>
2022-06-30 12:18:57 +0530

    backButton

--- src/components/data_visualization/imageDisplay/dev1.jsx
+++ src/components/data_visualization/imageDisplay/dev1.jsx
@@ -8,8 +8,8 @@ const Dev1 = () => {
   const history = useNavigate()
   return (
     <>
-      <button className="butt" onClick={() => history(-1)}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-back" viewBox="0 0 16 16">
-  <path d="M0 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v2h2a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2v-2H2a2 2 0 0 1-2-2V2zm2-1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H2z"/>
+      <button className="butt" onClick={() => history(-1)}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
 </svg></button>
       <div className="container-fluid9 p-0 d-flex flex-row">
         <Sidebar />
