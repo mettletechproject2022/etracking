540aa2bf0b9bbe246d6db11876feb7a1dd1a4bbb
mondal <mettletech.employee@gmail.com>
2022-07-04 16:54:27 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/login/login";
 import Home from "./components/home/home";
 import Forgot from "./components/login/forgotPass/forgot";
@@ -7,88 +8,43 @@ import Popupbutt from "./components/login/popUpButt/popUpButt";
 import ResetPassword from "./components/login/resetPassword/resetPassword";
 import Register from "./components/register/register";
 import Edit from "./components/edit/edit";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import ManageUser from "./components/manageuser/ManageUser";
-import DvUser from "./components/data_visualization/imageDisplay/user";
-import Dev2 from "./components/data_visualization/imageDisplay/dev2";
-import Dev3 from "./components/data_visualization/imageDisplay/dev3";
-import Dev4 from "./components/data_visualization/imageDisplay/dev4";
-import DataVisualisation from "../src/pages/DataVisualisation";
-import Dev1 from "./components/data_visualization/imageDisplay/dev1";
-import Unauth from "../src/pages/unauth";
-import Datetime from "./pages/Datetime";
+import Profile from "./components/profile/Profile";
+import Logout from "./components/logout/Logout";
+import Unauth from "./components/unauth/unauth";
 
+import ManageUser from "./components/manageuser/ManageUser";
+import DataVisualisation from "./components/dataVisualisation/DataVisualisation";
 
-
-
-import List from "./components/newManageuser/List"; 
-// import Edit from "./components/newManageuser/Edit"; 
-import User from "./components/newManageuser/User"; 
-import Header from "./components/newManageuser/Header"; 
-
-
-
-
-
-
-// for sidebar
-// import Sidebar from "./components/sidebar/Sidebar";
-
-// import DataVisualisation from "./pages/DataVisualisation";
-import History from "./pages/History";
-import Notification from "./pages/Notification";
-// import ManageUser from "./pages/ManageUser";
-import Profile from "./pages/Profile";
-import Logout from "./pages/Logout";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
-  const adminEmail = "raj@gmail.com";
-  const userEmail = "atanu@gmail.com";
-
   return (
-    <div>
+    <div className="container-fluid">
       <BrowserRouter>
         <Routes>
+          
           <Route exact path="/" element={<Login />} />
           <Route path="/home" element={<Home />} />
           <Route path="/forgot" element={<Forgot />} />
-          <Route path="/changePass" element={<ChangePassword />} />
+          <Route path="/changepass" element={<ChangePassword />} />
           <Route path="/otp" element={<OTP />} />
           <Route path="/resetPassword" element={<ResetPassword />} />
           <Route path="/popupbutt" element={<Popupbutt />} />
-          {/* <Route path="/data_visualization" element={<Data_visualization />} /> */}
-          <Route path="/dev1" element={<Dev1 />} />
-          <Route path="/dev2" element={<Dev2 />} />
-          <Route path="/dev3" element={<Dev3 />} />
-          <Route path="/dev4" element={<Dev4 />} />
-          <Route path="/register" element={<Register />} />
-          {/* <Route path="/edit" element={<Edit />} /> */}
-          <Route path="/dvUser" element={<DvUser />} />
-          <Route path="/dataVisualisation" element={<DataVisualisation />} />
-          <Route path="/history" element={<History />} />
+
           <Route path="/logout" element={<Logout />} />
-          <Route path="/user" element={<User />} />
 
-          {/* {localStorage.getItem("data") &&
-          JSON.parse(localStorage.getItem("data")).usertype === "Admin" ? ( */}
-          
-
-
-            {/* <Route path="/List" element={<List />} />
-
-
-
-
-          //  : (
-           
-
-          //   <Route path="/List" element={<Unauth />} />
-
-          // ) */}
-
-          <Route path="/notification" element={<Notification />} />
           <Route path="/profile" element={<Profile />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/edit" element={<Edit />} />
+
+          <Route exact path="/manageuser" element={<ManageUser />} />
+          {/* {localStorage.getItem("userdetails")!==null&&JSON.parse(localStorage.getItem("userdetails")).userType==="Admin" ? (
+            <Route path="/manageUser" element={<ManageUser />} />
+          ) : (
+            <Route path='/manageUser' element={<Unauth/>}/>
+          )} */}
+          <Route path="/datavisualisation" element={<DataVisualisation />} />
         </Routes>
       </BrowserRouter>
     </div>
